937f7051cc9811220d832e99442c2ab566acc4c7
Andy Stewart <boss@airbladesoftware.com>
2013-11-07 09:51:11 +0100

    Refactor haystack tests.

--- test/haystack_test.rb
+++ test/haystack_test.rb
@@ -3,17 +3,26 @@ require_relative '../lib/haystack'
 
 class HaystackTest < MiniTest::Unit::TestCase
 
+  test 'score' do
+    haystack = Haystack.new('foo')
+    assert_equal 1, haystack.score([0,1,2])
+    assert_operator haystack.score([0, 1, 2]), :>, haystack.score([0, 1])
+    assert_operator haystack.score([0, 1]),    :>, haystack.score([0])
+  end
+
+  #
+  # implementation tests (delete once implementation complete)
+  #
+
   test 'sanity check' do
     assert_equal 6, Haystack.new('foo').send(:maximum_score)
     assert_equal 6, Haystack.new('foo').send(:absolute_score, [0,1,2])
-    assert_equal 1, Haystack.new('foo').score([0,1,2])
   end
 
   test 'partial match' do
     assert_equal 3, Haystack.new('foo').send(:absolute_score, [0,2])
     assert_equal 4, Haystack.new('foo').send(:absolute_score, [0,1])
     assert_equal 3, Haystack.new('foo').send(:absolute_score, [1,2])
-    assert_in_delta 0.66, Haystack.new('foo').score([0,1]), 0.01
   end
 
   test 'prefers start of words' do
@@ -28,10 +37,6 @@ class HaystackTest < MiniTest::Unit::TestCase
     assert_equal 2, Haystack.new('foobar').send(:absolute_score, [1, 3])
     assert_equal 3, Haystack.new('foobar').send(:absolute_score, [2, 3])
   end
-
-  #
-  # implementation tests (delete once implementation complete)
-  #
 
   test '#individual_letter_scores' do
     assert_equal [2],             Haystack.new('f').send(:individual_letter_scores)
